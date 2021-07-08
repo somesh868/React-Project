4c3480b6402495e2c8686580a2f39bd4b7a56da2
somesh868 <someshkumarswain354@gmail.com>
2021-07-08 18:34:07 +0530

    react project

---
+++ src/CourseList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function CourseList(){
+    return <h2>CourseList</h2>
+
+    
+}
+
+export default CourseList;
---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function List(){
+    return(
+        <ol>
+            <a href="https://java.com/en/download/help/whatis_java.html"><li>java</li></a>
+            <a href="https://www.python.org/"><li>python</li></a>
+            <a href="https://en.wikipedia.org/wiki/C%2B%2B"><li>C++</li></a>
+            <a href="https://en.wikipedia.org/wiki/Data_mining"><li>Data mining</li></a>
+        </ol>
+
+    );
+}
+export default List;
